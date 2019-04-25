110f0e37c5d5bb536671d5013f0adee5631d32c2
gavin <gavin.miles81@outlook.com>
2019-04-25 12:41:04 +0100

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { HeadComponent } from './head/head.component';
+import { browser } from 'protractor';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    PostsComponent,
+    UsersComponent,
+    DetailsComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
